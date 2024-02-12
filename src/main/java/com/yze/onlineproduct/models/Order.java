7933347a3c62184c353b56630c0336a22cf01e7b
yzekraoui <zekraouiyoussef@outlook.com>
2024-02-12 16:13:56 +0100

    add model Order to the project

---
+++ src/main/java/com/yze/onlineproduct/models/Order.java
@@ -0,0 +1,19 @@
+package com.yze.onlineproduct.models;
+
+import jakarta.persistence.Entity;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@SuperBuilder
+
+public class Order extends AbsractEntity{
+
+    private String codeOrder;
+    private Integer quantity;
+}
